80c1fb76645c140dc506d17c2c0184f28aaa4f4b
zht98 <70802119+zht98@users.noreply.github.com>
2026-09-22 13:47:25 +0800

    Update launcher.c

--- launcher.c
+++ launcher.c
@@ -1,7 +1,6 @@
 #include <windows.h>
-#include <stdio.h>
 
-int main() {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     const char* workDir = "I:\\SteamLibrary\\steamapps\\common\\RealRTCW";
     const char* exePath = "I:\\SteamLibrary\\steamapps\\common\\RealRTCW\\RealRTCW.x64.exe";
 
@@ -12,25 +11,24 @@ int main() {
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
 
-    // 使用 CreateProcess 启动程序
+    // 使用 CreateProcessA 启动目标程序并指定工作目录
     if (CreateProcessA(
-            exePath,     // 可执行文件路径
+            exePath,     // 可执行文件全路径
             NULL,        // 命令行参数
             NULL,        // 进程安全属性
             NULL,        // 线程安全属性
             FALSE,       // 句柄继承
             0,           // 创建标志
-            NULL,        // 使用父进程环境变量
+            NULL,        // 使用默认环境变量
             workDir,     // 设置工作目录
             &si, 
             &pi)) {
         
-        // 关闭不必要的句柄
+        // 成功启动后关闭不必要的句柄
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
         return 0;
-    } else {
-        printf("Error launching process: %lu\n", GetLastError());
-        return 1;
     }
+
+    return 1;
 }
